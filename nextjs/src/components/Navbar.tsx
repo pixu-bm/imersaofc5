1379d6c25e44a30799bab6401d60f94ac8ab1b27
Bruno Russo - Pixu <bruno_marco@hotmail.com>
2023-06-16 01:17:17 -0300

    v3

--- nextjs/src/components/Navbar.tsx
+++ nextjs/src/components/Navbar.tsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
       <Toolbar>
         <StoreIcon />
         <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
-          Fincycle
+          Gateway Pagamentos v3
         </Typography>
       </Toolbar>
     </AppBar>
